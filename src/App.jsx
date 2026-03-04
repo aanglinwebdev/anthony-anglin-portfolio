30bc5f1f31fb23f545b931825a126768c4e3149a
Anthony <aanglin07@gmail.com>
2026-03-04 05:22:31 -0500

    Fix import capitalization for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./styles/styles.css";
 
 import Header from "./components/Header";
-import About from "./components/about";
+import About from "./components/About";
 import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
 import Navbar from "./components/Navbar";
